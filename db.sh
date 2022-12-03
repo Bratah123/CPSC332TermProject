cfcda7ae6af4622cff6bba000d4973dc4a5725c3
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2022-12-03 09:23:41 +0800

    FEAT: Script to update DB

---
+++ db.sh
@@ -0,0 +1,31 @@
+# This script will drop old databases/tables and re-create them in the appropriate order
+# This script requires an option file to be set (see README), for provision of password (not over command line)
+
+# Helper Functions ------------------------------------------------------------------------------
+# For logging:
+timestamp() {
+    date +"%T"
+}
+
+# Constants---------------------------------------------------------------------------------------
+# SQL script dir
+location="/home/titan0/cs332g/cs332g20/homepage/sql"
+# Ordered Array:
+scripts=("$location/drop.sql"
+"$location/tables.sql"
+"$location/data.sql")
+# Default database:
+schema="cs332g20"
+
+echo "Now running SQL scripts"
+
+for val in ${scripts[@]}; do
+    timestamp >> log.txt
+    echo "    Running $val" >> log.txt
+    sshpass -f './password' ssh cs332g20@shell.ecs.fullerton.edu "mysql $schema < $val" >> log.txt
+done
+
+timestamp >> log.txt
+echo -e "    Sequence completed! \n---------------------------------------- \n" >> log.txt
+echo "" >> log.txt
+echo "Sequence completed! Check log.txt for details."
